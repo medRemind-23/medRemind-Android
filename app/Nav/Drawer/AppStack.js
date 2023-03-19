ba9bf3bc40fc71c99640679f8ba1b9cba0dc7692
10cyrilc <10cyrilc@gmail.com>
2023-03-19 16:03:50 +0530

    [Better] Better Code

--- app/Nav/Drawer/AppStack.js
+++ app/Nav/Drawer/AppStack.js
@@ -2,11 +2,9 @@ import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import CustomDrawer from "./CustomDrawer";
-import Login from "../../screens/Login";
 import Dashboard from "../../screens/Dashboard";
 import Entry from "../../screens/Entry";
 import History from "../../screens/History";
-import Push from "../../DB/Push";
 
 const Drawer = createDrawerNavigator();
 
@@ -30,7 +28,7 @@ const Appstack = () => {
       <Drawer.Screen name="Dashboard" component={Dashboard} />
       <Drawer.Screen name="Entry" component={Entry} />
       <Drawer.Screen name="History" component={History} />
-      <Drawer.Screen name="Push" component={Push} />
+      {/* <Drawer.Screen name="Push" component={Push} /> */}
     </Drawer.Navigator>
   );
 };
